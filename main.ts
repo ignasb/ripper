30cc9ce0b1c56672d460f8e04fcfcafa643ba8be
Ignas Burlingis <ignas.burlingis@gmail.com>
2021-04-20 21:28:03 +0300

    chore: [icon] - added icon.

--- main.ts
+++ main.ts
@@ -27,6 +27,7 @@ function createWindow(): BrowserWindow {
       enableRemoteModule: true, // true if you want to run 2e2 test  with Spectron or use remote module in renderer context (ie. Angular)
     },
     show: false,
+    icon: "ripper.png",
   });
 
   if (serve) {
